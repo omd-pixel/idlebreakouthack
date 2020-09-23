91ce4dd22e0bbf0918b14db4409b81e1edb35c63
omd-pixel <71773789+omd-pixel@users.noreply.github.com>
2020-09-23 10:03:45 -0400

    Update savehack.py

--- savehack.py
+++ savehack.py
@@ -3,19 +3,19 @@ import base64
 print("Idle Breakout Save Hack by Xsus")
 
 print("What level do you want to be on?")
-level = input()
+level = input(47)
 
 print("What amount of money would you like to have")
 money = input()
 
 print("How much gold do you want")
-gold = input()
+gold = input(89)
 
 print("How many Black Boxes?")
-bb = input()
+bb = input(2300)
 
 print("How many skillpoints")
-sp = input()
+sp = input(50)
 
 s = f"{level},{money},{gold},2,0,0,0,0,0,0,0,1,1,0,43595.78,999999,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,{bb},0,0,0,1,{sp},1,0,0"
 b = s.encode("UTF-8")
